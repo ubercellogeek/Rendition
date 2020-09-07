fa1c997e0643faecf095368fc312b886e1ef4862
Stephen Strand <sstrand@int.dotnetdev.io>
2020-09-07 15:25:06 -0500

    Updated folder locations

--- src/Rendition.Server/Configuration/RenditionServerConfiguration.cs
+++ src/Rendition.Server/Configuration/RenditionServerConfiguration.cs
@@ -4,8 +4,8 @@ namespace Rendition.Server.Configuration
 {
     public class RenditionServerConfiguration
     {
-        public string InstallPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "install");
-        public string TempDataPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "temp");
-        public string OutputPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "data");
+        public string InstallPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), ".local-data\\install");
+        public string TempDataPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), ".local-data\\temp");
+        public string OutputPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), ".local-data\\output");
     }
 }
